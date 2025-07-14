72f21aed53508db48fd2bb64573d3058f50d8342
Daria J. <frontendwebcraftx@gmail.com>
2025-07-14 13:47:17 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -23,7 +23,7 @@ dropdown.addEventListener('change', (event) => {
 // Add an event listener to the continue button
 continueButton.addEventListener('click', () => {
    
-    window.location.href = '/budget.html'; 
+    window.location.href = 'Javascript_journey/budget.html'; 
     localStorage.setItem('chosenAssistant', dropdown.value);
 });
 
