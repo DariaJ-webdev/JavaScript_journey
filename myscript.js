2ff4ca61c893c610c5168eef58d32e4c0559ff13
DariaJ-webev <frontendwebcraftx@gmail.com>
2025-10-21 21:36:59 -0400

    updated relative paths

--- myscript.js
+++ myscript.js
@@ -5,28 +5,24 @@ const confirmationMessageElement = document.getElementById('confirmationMessage'
 const continueButton = document.getElementById('continueButton');
 
 dropdown.addEventListener('change', (event) => {
-    const selectedAssistant = event.target.value;
+const selectedAssistant = event.target.value;
 
-    if (selectedAssistant === "") { 
-        confirmationMessageElement.textContent = "Please make a valid selection.";
-        confirmationMessageElement.style.color = "red";
-        continueButton.style.display = "none"; // Hide the button
-    } else {
-        confirmationMessageElement.textContent = `Great! You have chosen ${selectedAssistant}! Click 'Get started' to proceed.`;
-        confirmationMessageElement.style.color = "green";
-        continueButton.style.display = "block"; 
-    }
+if (selectedAssistant === "") { 
+confirmationMessageElement.textContent = "Please make a valid selection.";
+confirmationMessageElement.style.color = "red";
+continueButton.style.display = "none";
+} else {
+confirmationMessageElement.textContent = `Great! You have chosen ${selectedAssistant}! Click 'Get started' to proceed.`;confirmationMessageElement.style.color = "green";
+continueButton.style.display = "block"; 
+ }
 
-    
+
 });
 
-// Add an event listener to the continue button
+// Added an event listener to the continue button
 continueButton.addEventListener('click', () => {
-   
-    window.location.href = '/JavaScript_journey/budget.html'; 
-    localStorage.setItem('chosenAssistant', dropdown.value);
+window.location.href = 'budget.html'; 
+localStorage.setItem('chosenAssistant', dropdown.value);
 });
 
 //End of Welcome Page//
-
-
